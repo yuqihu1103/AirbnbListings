5a97fa73e4af9a3af5d697938edeb6b79a6ecf93
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-03 18:01:22 -0700

    fetch listing data from json file

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("airbnb_sf_listings_500.json")
     .then((response) => response.json())
     .then((data) => {
-      // 'data' is an array of objects
-      data.forEach((item) => {
-        console.log(item);
-      });
+      let listings = document.getElementById("listings");
+      for (let i = 0; i < 50; i++) {
+        let listing = data[i];
+        listings.innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Listing Name</h5>
+                <p class="card-text">Description</p>
+                <p class="card-text">Ameneties</p>
+                <p class="card-text">host</p>
+                <p class="card-text">price</p>
+                <p class="card-text">thumbnail</p>
+            </div>
+        </div>
+        `;
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
